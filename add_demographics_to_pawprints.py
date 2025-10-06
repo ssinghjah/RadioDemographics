4109a35a85f5f4a49c5c3b43fa1c677d500d808e
Simran Singh <ssingh28@ncsu.edu>
2025-10-06 17:59:55 -0400

    Revert "Process segments given start and offset"
    This reverts commit 528ec4be60cfa6cc09f987f7ef3181f4b0310648.

--- add_demographics_to_pawprints.py
+++ add_demographics_to_pawprints.py
@@ -31,8 +31,8 @@ def _county_to_fips(county_name, fips_map):
 
 parser = argparse.ArgumentParser(description = "Adds county, census tract, and census block geo ids to each log row, using which latest demographic info are added from US census database.")
 
-PAWPRINTS_ROOT_FOLDER = "/home/ubuntu/RadioDemographics/data/split/"
-PAWPRINTS_OUTPUT_FOLDER = "~/RadioDemographics/data/split_geoids/"
+PAWPRINTS_ROOT_FOLDER = "/home/ubuntu/pawprints-processing/data/split_interpolated/"
+PAWPRINTS_OUTPUT_FOLDER = "~/pawprints-processing/data/split_geoids/"
 SLEEP = 0.2
 
 def process_file(input_path, output_path):
@@ -75,17 +75,16 @@ def process_file(input_path, output_path):
         pawprints_pd.to_csv(output_path, index=False)
 
 
-START = 1
+START = 0
 END = 79
-OFFSET = 4
 PREFIX = "aerpaw-1_cellular_interpolated_seg_"
-current_seg = START
-while current_seg <= END:
-    print("Segment:" + str(current_seg))
-    input_file_path = os.path.join(PAWPRINTS_ROOT_FOLDER, PREFIX + str(current_seg) + ".csv")
-    output_path = os.path.join(PAWPRINTS_OUTPUT_FOLDER, PREFIX + str(current_seg) + "_geoids.csv")
+for i in range(START, END + 1):
+    if i % 4 != 0:
+        continue
+    print(i)
+    input_file_path = os.path.join(PAWPRINTS_ROOT_FOLDER, PREFIX + str(i) + ".csv")
+    output_path = os.path.join(PAWPRINTS_OUTPUT_FOLDER, PREFIX + str(i) + "_geoids.csv")
     process_file(input_file_path, output_path)
-    current_seg += OFFSET
 
 
 def _rucc_from_fips(fips, rucc_pd):
